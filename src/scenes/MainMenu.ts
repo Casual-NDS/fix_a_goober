0993605c091b6e076f9e132357007a07c8db7f40
Casual-NDS <robert.martinez@student.tamdistrict.org>
2025-03-17 22:19:57 +0000

    title change wooooo

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -13,15 +13,11 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.background = this.add.image(800, 600, 'background');
+        this.background = this.add.image(400, 300, 'background');
 
-        this.logo = this.add.image(400, 300, 'logo');
+        
 
-        this.title = this.add.text(412, 360, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        
 
         this.input.once('pointerdown', () => {
 
